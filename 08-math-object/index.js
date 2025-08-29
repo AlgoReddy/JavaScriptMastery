8239f5fac5b2f21d384e8cedf02995a79edbae39
algoreddy <helloyash99@gmail.com>
2025-08-30 04:56:51 +0530

    learned about the important math functions in the javascript

---
+++ 08-math-object/index.js
@@ -0,0 +1,36 @@
+// math = build in object that provides a
+//        collection of properties  and methods
+
+
+let x=89.4;
+let y=2;
+let z =4;
+
+// z=Math.round(y);
+// z=Math.round(x);  // rounds to the nearest number lower until 4 & >4 rounds to next number
+// z=Math.ceil(x);
+// z=Math.floor(x);
+// z=Math.trunc(x); // eliminates all the decimal numbers
+// z=Math.pow(x,y);
+// z=Math.sqrt(x);
+// z=Math.log(x);
+// z=Math.sin(x);
+// z=Math.cos(x);
+// z=Math.tan(x);
+// z=Math.abs(x); // make it a positive integer
+// z=Math.sign(x);  // +ve -->1  , 0 --> 0 , -ve --> -1
+let max = Math.max(x , y , z);
+let min = Math.min(x , y , z );
+console.log(min);
+console.log(max)
+
+
+
+
+
+
+
+
+
+
+console.log(z);
