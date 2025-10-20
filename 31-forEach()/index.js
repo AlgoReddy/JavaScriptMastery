b1ccb28f5d6e901e03fee782460d13f03c135224
algoreddy <helloyash99@gmail.com>
2025-10-21 00:29:38 +0530

    done js objects basics

--- 31-forEach()/index.js
+++ 31-forEach()/index.js
@@ -1,4 +1,16 @@
-let a = 2;
-const {bar} = a ;
-console.log(bar);
+// forEach() = method used to iterate over the elements of the array & apply a specified  function (callback ) to each element
+// array.forEach(callback);
 
+const nums = [1, 2, 3, 4, 5];
+const doubles = nums.forEach((element , index , array)=>array[index]=element*9);
+nums.forEach(display);
+
+function triple(element, index, array) {
+  array[index] = element * 3;
+}
+function double(element, index, array) {
+  array[index] = element * 2;
+}
+function display(element) {
+  console.log(element);
+}
--- 37-javascriptObjects/index.js
+++ 37-javascriptObjects/index.js
@@ -1,4 +1,15 @@
-let a = 2;
-const {bar} = a ;
-console.log(bar);
+// objects = collections of the related properties  and or methods
+//           can represent a real world entity or object( car , human , vehicles  , places etc )
+//  object = {key:value , or function()}
 
+const person1 = {
+    firstName:"marcus",
+    lastName :"aurelias",
+    age:24,
+    martialStatus:"single",
+    favoriteItem:"perugu",
+    eat:function(){return `${this.firstName} eats ${this.favoriteItem}`},
+    sleep:()=>`${this.firstName}${this.lastName} is hard to sleep during day & night also` ,
+
+}
+console.log(person1.eat());
