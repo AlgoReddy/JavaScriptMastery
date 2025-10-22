1f8a7cd5a9a1e13a49486155c7b9dbac37973881
algoreddy <helloyash99@gmail.com>
2025-10-22 14:44:42 +0530

    completed the classes in js

--- 40-classes/index.js
+++ 40-classes/index.js
@@ -1,24 +1,17 @@
-//declaration let x ;
-//assigning x = 100;
+// class = (ES6 feature)  provides a more structured  and cleaner way to work with objects compared to tradtional
+//         constructor functions
+//          ex:static  ,encapsulation  , inheritance
 
-// let age = 25 ;
-// let price = 10.99;
-// let gpa=2.1;
-// let firstName= "Yash Bro ";
-// let favoriteFood= "chicken Biryani";
-// let email = "helloyash99@gmail.com";
-// console.log(firstName);
-// console.log(typeof firstName);
-// console.log(`Your name is ${firstName}`);
-// console.log(`You are ${age} years old`);
-// console.log(`The price is $${price}`);
-// console.log(`your gpa is ${gpa}`);
-// console.log(`You like ${favoriteFood}`);
-
-let fullName = "Yash";
-let age = 25;
-let isStudent = false;
-
-document.getElementById("p1").textContent = `Your name is ${fullName}`;
-document.getElementById("p2").textContent = `your age is ${age} years old`;
-document.getElementById("p3").textContent = `are you a student ${isStudent}`;
+class Product {
+  constructor(price, name) {
+    this.price = price;
+    this.name = name;
+  }
+  displayProduct() {
+    console.log(
+      `product name is ${this.name} & price of that item ${this.price} ruppees `
+    );
+  }
+}
+const product = new Product(899898, "Honda Unicorn");
+product.displayProduct();
