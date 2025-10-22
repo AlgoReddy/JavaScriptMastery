d9321249024dff74d045a762f5d7980b88fe43ab
algoreddy <helloyash99@gmail.com>
2025-10-22 11:47:29 +0530

    implemented classes

---
+++ 40-classes/index.js
@@ -0,0 +1,24 @@
+//declaration let x ;
+//assigning x = 100;
+
+// let age = 25 ;
+// let price = 10.99;
+// let gpa=2.1;
+// let firstName= "Yash Bro ";
+// let favoriteFood= "chicken Biryani";
+// let email = "helloyash99@gmail.com";
+// console.log(firstName);
+// console.log(typeof firstName);
+// console.log(`Your name is ${firstName}`);
+// console.log(`You are ${age} years old`);
+// console.log(`The price is $${price}`);
+// console.log(`your gpa is ${gpa}`);
+// console.log(`You like ${favoriteFood}`);
+
+let fullName = "Yash";
+let age = 25;
+let isStudent = false;
+
+document.getElementById("p1").textContent = `Your name is ${fullName}`;
+document.getElementById("p2").textContent = `your age is ${age} years old`;
+document.getElementById("p3").textContent = `are you a student ${isStudent}`;
