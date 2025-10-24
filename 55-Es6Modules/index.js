c204a11d9260e9b2351250971cee099c99c17eb8
algoreddy <helloyash99@gmail.com>
2025-10-24 23:03:04 +0530

    es6 modules completed

---
+++ 55-Es6Modules/index.js
@@ -0,0 +1,8 @@
+import {PI,getCircumference,getArea,getVolume} from './moduleEx.js';
+
+const circumeference = getCircumference(23);
+console.log(circumeference);
+const area= getArea(23);
+console.log(area);
+const volume = getVolume(23);
+console.log(`volume of the sphere is ${volume} cm^3`);
